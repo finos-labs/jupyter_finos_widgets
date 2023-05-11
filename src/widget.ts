15d8d8d09e64c071dfe79d13444c95466a1fd308
ncucciniello <ncucciniello@gmail.com>
2023-05-11 16:08:21 -0400

    replace open news with raise intent widget Signed-off-by: Nico Cucciniello <ncucciniello@gmail.com>

--- src/widget.ts
+++ src/widget.ts
@@ -9,5 +9,5 @@ export * from './FDC3ConnectionStatus';
 export * from './FDC3ChannelJoin';
 export * from './FDC3TickerInput';
 export * from './FDC3ContextDisplay';
-export * from './FDC3ViewNews';
+export * from './FDC3RaiseIntent';
 export * from './ExampleWidget';
