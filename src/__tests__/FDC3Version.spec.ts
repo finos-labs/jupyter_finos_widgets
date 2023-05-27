74d60b25a392eec15efe597cec81be5b8f07ed03
Chuck Danielsson <chuck.danielsson@gmail.com>
2023-05-27 12:34:55 -0400

    FDC3Version model tests with mocks. Dependency injection
    Signed-off-by: Chuck Danielsson <chuck.danielsson@gmail.com>

---
+++ src/__tests__/FDC3Version.spec.ts
@@ -0,0 +1,99 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import { mock } from 'jest-mock-extended'
+import { DesktopAgent } from '@finos/fdc3'
+
+import { createTestModel } from './utils'
+
+import {
+  FDC3ReadyErrorMessage,
+  FDC3VersionModel,
+  FDC3_NOT_FOUND_MESSAGE,
+  FDC3_READY_UNKNOWN_ERROR,
+  FDC3_VERSION_DEFAULT,
+} from '..'
+
+describe('FDC3VersionModel', () => {
+  describe('when the fdc3 global is not available', () => {
+    it('has the correct defaults', () => {
+      const testModel = createTestModel(FDC3VersionModel)
+      expect(testModel).toBeInstanceOf(FDC3VersionModel)
+      expect(testModel.get('fdc3Version')).toEqual(FDC3_VERSION_DEFAULT)
+    })
+
+    it('uses the not found message when the fdc3 timeout error occurs', (done) => {
+      const mockReady = async () => {
+        throw new Error(FDC3ReadyErrorMessage)
+      }
+      const testModel = createTestModel(FDC3VersionModel, {}, mockReady)
+
+      testModel.once('change:fdc3Version', () => {
+        expect(testModel.get('fdc3Version')).toEqual(FDC3_NOT_FOUND_MESSAGE)
+        done()
+      })
+    })
+
+    it('uses the unknown error message when another error occurs', (done) => {
+      const mockReady = async () => {
+        throw new Error(Math.random().toString())
+      }
+      const testModel = createTestModel(FDC3VersionModel, {}, mockReady)
+
+      testModel.once('change:fdc3Version', () => {
+        expect(testModel.get('fdc3Version')).toEqual(FDC3_READY_UNKNOWN_ERROR)
+        done()
+      })
+    })
+
+    it('uses the unknown error message something other than an error is thrown', (done) => {
+      const mockReady = async () => {
+        throw Math.random().toString()
+      }
+      const testModel = createTestModel(FDC3VersionModel, {}, mockReady)
+
+      testModel.once('change:fdc3Version', () => {
+        expect(testModel.get('fdc3Version')).toEqual(FDC3_READY_UNKNOWN_ERROR)
+        done()
+      })
+    })
+
+    it('works correctly in the vanilla jsdom where no fdc3 global exists', (done) => {
+      const testModel = createTestModel(FDC3VersionModel, {}, undefined, 1)
+
+      testModel.once('change:fdc3Version', () => {
+        expect(testModel.get('fdc3Version')).toEqual(FDC3_NOT_FOUND_MESSAGE)
+        done()
+      })
+    })
+  })
+
+  describe('when the fdc3 global is available', () => {
+    it('stores the returned version number from global fdc3 call', (done) => {
+      const mockFDC3 = mock<DesktopAgent>()
+      const randomVersion = Math.random().toString()
+
+      mockFDC3.getInfo.calledWith().mockResolvedValue({
+        fdc3Version: randomVersion,
+        provider: 'Mock',
+        optionalFeatures: {
+          OriginatingAppMetadata: false,
+          UserChannelMembershipAPIs: false,
+        },
+        appMetadata: {
+          appId: 'Mock Id',
+        },
+      })
+
+      window.fdc3 = mockFDC3
+
+      const testModel = createTestModel(FDC3VersionModel)
+      testModel.once('change:fdc3Version', () => {
+        expect(
+          testModel.get('fdc3Version').includes(randomVersion)
+        ).toBeTruthy()
+        done()
+      })
+    })
+  })
+})
